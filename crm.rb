e6b8b2a477e6858fe8fc0c2418c95f4846e26a5c
Alec Bollard <alecbollard@gmail.com>
2015-08-28 10:41:09 -0400

    finished the basics of the CRUD program

--- crm.rb
+++ crm.rb
@@ -4,6 +4,9 @@ require 'sinatra'
 
 $rolodex= Rolodex.new
 
+# temp data
+$rolodex.add_contact(Contact.new("Johnny", "Bravo", "johnny@bitmakerlabs.com", "Rockstar"))
+
 get '/' do
   @crm_app_name = "My CRM"
   erb :index
@@ -15,6 +18,55 @@ end
 
 get '/contacts/new' do
   erb :new_contact
+end
+
+get "/contacts/:id" do
+  @contact = $rolodex.find(params[:id].to_i)
+  if @contact
+    erb :show_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+get "/contacts/:id/edit" do
+  @contact = $rolodex.find(params[:id].to_i)
+  if @contact
+    erb :edit_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+put "/contacts/:id" do
+  @contact = $rolodex.find(params[:id].to_i)
+  if @contact
+    @contact.first_name = params[:first_name]
+    @contact.last_name = params[:last_name]
+    @contact.email = params[:email]
+    @contact.note = params[:note]
+
+    redirect to("/contacts")
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+delete "/contacts/:id" do
+  @contact = $rolodex.find(params[:id].to_i)
+  if @contact
+    $rolodex.remove_contact(@contact)
+    redirect to("/contacts")
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+get "/delete_contact" do
+	<form action="/contacts/<%= @contact.id %>" method="post">
+	  <input type="hidden" name="_method" value="delete">
+	  <input type="submit" name="Delete Contact">
+	</form>
 end
 
 post '/contacts' do
